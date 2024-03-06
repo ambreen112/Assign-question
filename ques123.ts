59fb03812c6503d310cfa0fc713919289de795ed
ambreen112 <160263381+ambreen112@users.noreply.github.com>
2024-03-06 21:30:21 +0500

    Add files via upload

---
+++ ques123.ts
@@ -0,0 +1,15 @@
+ // Question No # 2
+
+ let _Pnoun = "Hina";
+  console.log ("Hello", _Pnoun,"would you like to come with me?");
+
+ //Question no # 3
+
+ let PersonName = "Sania Saeed";
+  console.log(PersonName.toLowerCase());
+ console.log(PersonName.toUpperCase());
+ console.log(PersonName.replace(/\b\w/g, c => c.toUpperCase()));
+
+// Question No # 4
+
+console.log("Hazrat Ali said,Calmness is a sign of intelligence.");
