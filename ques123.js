18ee559f7a873880e36e49b3eb841eb4e92ed673
ambreen112 <160263381+ambreen112@users.noreply.github.com>
2024-03-06 21:31:09 +0500

    Add files via upload

---
+++ ques123.js
@@ -0,0 +1,10 @@
+// Question No # 2
+var _Pnoun = "Hina";
+console.log("Hello", _Pnoun, "would you like to come with me?");
+//Question no # 3
+var PersonName = "Sania Saeed";
+console.log(PersonName.toLowerCase());
+console.log(PersonName.toUpperCase());
+console.log(PersonName.replace(/\b\w/g, function (c) { return c.toUpperCase(); }));
+// Question No # 4
+console.log("Hazrat Ali said,Calmness is a sign of intelligence.");
