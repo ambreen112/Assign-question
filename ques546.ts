6759183c74c33de043b2aec709f30e00dca76c25
ambreen112 <160263381+ambreen112@users.noreply.github.com>
2024-03-06 21:31:43 +0500

    Add files via upload

---
+++ ques546.ts
@@ -0,0 +1,11 @@
+//Question No # 5
+let famous_person="Hazrat Ali";
+let message="Calmness is a sign of intelligence";
+console.log(famous_person,message);
+
+//Question No # 6
+
+let withwhitespace= "\n\t Mahanoor\t\n";
+console.log(withwhitespace);
+let withoutspace="Mahanoor";
+console.log(withoutspace);
