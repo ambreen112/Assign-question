c624b92b9d228e9064360a0dec72b628b8806e45
ambreen112 <160263381+ambreen112@users.noreply.github.com>
2024-03-07 11:18:14 +0500

    Add files via upload

---
+++ ques12.js
@@ -0,0 +1,8 @@
+//Question No# 12
+var i;
+var Namefrinds = ["Subia", "Ammara", "Sahar", "Nargis"];
+var message = "Would you come tomorrow,";
+for (var _i = 0, Namefrinds_1 = Namefrinds; _i < Namefrinds_1.length; _i++) {
+    i = Namefrinds_1[_i];
+    console.log(message + i);
+}
