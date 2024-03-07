f2117b336bc71533d92a51660451638a8384d70c
ambreen112 <160263381+ambreen112@users.noreply.github.com>
2024-03-07 21:37:31 +0500

    Add files via upload

---
+++ ques15.ts
@@ -0,0 +1,11 @@
+
+// Question No # 15
+let GuestList:String[]=["Sana","Maha","Noor"];
+
+let GuestNotCome =GuestList[0];
+
+console.log(GuestNotCome, "me nai a sakta");
+
+GuestList.splice(0 ,0 "Neha");
+
+GuestList.forEach(Guest => console.log(`Respected madam ${Guest},You are invited for dinner?`);)
