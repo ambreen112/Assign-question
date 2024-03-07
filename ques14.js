1e11690314e3838aa2c0d7771902c080fb1d044e
ambreen112 <160263381+ambreen112@users.noreply.github.com>
2024-03-07 12:12:14 +0500

    Add files via upload

---
+++ ques14.js
@@ -0,0 +1,6 @@
+//Question No # 14
+var i;
+var GuestList = ["Sana", "Maha", "Noor"];
+for (i = 0; i < GuestList.length; i++) {
+    console.log('Respected Madam' + GuestList[i] + '\n you are invited for dinner.\n');
+}
